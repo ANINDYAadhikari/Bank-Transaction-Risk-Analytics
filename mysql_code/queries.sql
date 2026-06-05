bf4172cacec82401d827b3ee7f6ce746cca5503d
ANINDYAadhikari <anindyaadhikari140@gmail.com>
2026-06-05 19:36:27 +0530

    Improvements

--- mysql_code/queries.sql
+++ mysql_code/queries.sql
@@ -14,3 +14,13 @@ GROUP BY AccountID
 ORDER BY total_spend DESC
 LIMIT 20;
 
+-- Analyze transaction volume and spending by transaction type
+-- Which transaction types generate the most transactions and highest total transaction amounts?
+SELECT
+    TransactionType,
+    COUNT(*) AS transaction_count,
+    ROUND(SUM(TransactionAmount), 2) AS total_amount,
+    ROUND(AVG(TransactionAmount), 2) AS avg_amount
+FROM transactions
+GROUP BY TransactionType
+ORDER BY total_amount DESC;
